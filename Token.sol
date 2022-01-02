8b30a9c2d18919e3747368f8749f116859e2491c
ZiadNet <35084595+ZiadNet@users.noreply.github.com>
2022-01-02 04:57:05 +0100

    Update Token.sol
    http://remix.ethereum.org/

--- Token.sol
+++ Token.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.0.1;
 contract Token {
     mapping(address => uint) public balances;
     mapping(address => mapping(address => uint)) public allowance;
-    uint public totalSupply = 10000 * 10 ** 18;
-    string public name = "My Token";
-    string public symbol = "TKN";
+    uint public totalSupply = 15000000 * 10 ** 18;
+    string public name = "Norsk Token";
+    string public symbol = "NOK";
     uint public decimals = 18;
     
     event Transfer(address indexed from, address indexed to, uint value);
